a5abe113702b2728846bce4135266ba6e924bcc5
Jasmin Hudacsek <jasmin@hudacsek.com>
2018-05-19 15:01:23 -0600

    Add user sign up feature spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  get '/signup' => 'users#new'
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tweets, only: [:index]
 end
--- spec/features/user_can_login.rb
+++ spec/features/user_can_login.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
 
-RSpec.feature "The login process", type: :feature do
-  before :each do
-    User.make(email: 'user@example.com', password: 'password')
-  end
+RSpec.feature "The login process", type: :feature do 
 end
