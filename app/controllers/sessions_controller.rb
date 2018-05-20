80f2d75e3801d48b055507846e1c063c9455228a
Jasmin Hudacsek <jasmin@hudacsek.com>
2018-05-20 12:21:38 -0600

    Add user login error functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,9 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             redirect_to tweets_path, notice: "Logged in!"
+        else
+            flash.now[:danger] = "Username and/or Password is invalid. Try again."
+            render :new
         end
     end
 end
