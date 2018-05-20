830ba27fc6f57f5e1c039ab01823b251c6bd205f
Jasmin Hudacsek <jasmin@hudacsek.com>
2018-05-20 15:44:28 -0600

    Update tweet spec

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-    it 'has a created at time' do
+    it 'has a created at date and time' do
         raw_tweet = Faker::Twitter.status
         tweet = Tweet.new(raw_tweet)
 
-        expect(tweet.created_at.to_s).to eq(raw_tweet[:created_at])
+        expect(tweet.created_at[0..9]).to eq(raw_tweet[:created_at][0..9])
+    end
+
+    xit 'has content' do
+        raw_tweet = Faker::Twitter.status
+        tweet = Tweet.new(raw_tweet)
+
+        expect(tweet.text).to eq(raw_tweet[:text])
     end
 end
