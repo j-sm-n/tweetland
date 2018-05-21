12d8f6a979e641ecb1a98844153f9c8e87c24e93
Jasmin Hudacsek <jasmin@hudacsek.com>
2018-05-20 20:44:42 -0600

    Reformat tweet.rb and add model spec back

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,11 +3,12 @@ class Tweet
                   :text
     
     def initialize(raw_tweet)
-        @created_at = format_time(raw_tweet.created_at)
-        @text       = raw_tweet.text
+        @created_at = format_time(raw_tweet[:created_at])
+        @text       = raw_tweet[:text]
     end
 
     def format_time(raw_time)
-        raw_time.strftime('%a %b %d, %Y %I:%M%P')
+        datetime = DateTime.parse(raw_time)
+        datetime.strftime('%a %b %d, %Y %I:%M%P')
     end
 end
